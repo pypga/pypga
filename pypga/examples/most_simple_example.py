a01d03c92679543243d4b1e9d0baa5a289b90123
Leonhard <leonhard.neuhaus@gmail.com>
2023-01-19 11:31:54 +0100

    thomas example

---
+++ pypga/examples/most_simple_example.py
@@ -0,0 +1,37 @@
+from migen import Signal
+from pypga.core import TopModule, Module, logic, Register
+
+
+class BlinkLED(TopModule):
+   # register accessible from Python
+   rate: Register.custom(width=8)
+
+   # FPGA Code
+   @logic
+   def _blink(self, platform):
+       self.counter = Signal(8)
+       self.sync += [self.counter.eq(self.counter + self.rate)]
+       self.sync += [platform.request("user_led").eq(self.counter[-1])]
+
+   # on PC
+   def speed_up(self):
+       self.rate *= 2
+
+board = BlinkLED.run(host="10.153.153.255", board="stemlab125_14")
+
+print(board.rate)
+
+board.rate = 5
+board.speed_up()
+
+
+
+class BlinkLED(TopModule):
+   rate: Register.custom(width=8)
+
+   @logic
+   def blink(self, platform):
+       self.counter = Signal(8)
+       self.sync += [self.counter.eq(self.counter + self.rate)]
+       self.sync += [platform.request("user_led").eq(self.counter[-1])]
+
