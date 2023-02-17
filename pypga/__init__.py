97f1d663075c5326f086852455d903be43332c4b
josselex <jossele@gmx.de>
2023-02-17 10:20:14 +0100

    more comments / changes

--- pypga/__init__.py
+++ pypga/__init__.py
@@ -6,5 +6,5 @@ from . import boards, core, modules
 from .core import interface
 
 class config:
-    user = os.getenv("REDPITAYA_USERNAME", "root"),
+    user = os.getenv("REDPITAYA_USERNAME", "root")
     password = os.getenv("REDPITAYA_PASSWORD", "root")
