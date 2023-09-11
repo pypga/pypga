00f012d0ab0de5f8e491dc02d185f824869aaf6d
josselex <jossele@gmx.de>
2023-09-11 16:57:12 +0200

    fixed PID

--- pypga/modules/migen/pid.py
+++ pypga/modules/migen/pid.py
@@ -14,6 +14,7 @@ class MigenPid(MigenModule):
         k_p,
         k_i,
         width: int = 16,
+        gainbits: int=24
     ):
         """
         PI controller with saturation.
@@ -33,13 +34,13 @@ class MigenPid(MigenModule):
 
         # need to figure out the width of all these signals - probably should be more "tunable", in the sense that gains can have different width than signals
         self.input_signal = Signal(bits_sign=(width, True), reset=0)
-        self.k_p_signal = Signal(bits_sign=(width, True), reset=0)
-        self.k_i_signal = Signal(bits_sign=(width, True), reset=0)
+        self.k_p_signal = Signal(bits_sign=(gainbits, True), reset=0)
+        self.k_i_signal = Signal(bits_sign=(gainbits, True), reset=0)
         self.setpoint_signal = Signal(bits_sign=(width, True), reset=0)
-        self.prop = Signal(bits_sign=(2*width+1, True), reset=0)
-        self.int = Signal(bits_sign=(2*width+1, True), reset=0)
-        self.int_sum = Signal(bits_sign=(2*width+1, True), reset=0)
-        self.sum = Signal(bits_sign=(2*width+2, True), reset=0)
+        self.prop = Signal(bits_sign=(width, True), reset=0)
+        self.int = Signal(bits_sign=(30+width, True), reset=0)
+        self.int_sum = Signal(bits_sign=(30+width, True), reset=0)
+        #self.sum = Signal(bits_sign=(30+width, True), reset=0)
 
         # make sure all signals have correct width and signedness
         self.comb += [
@@ -48,11 +49,17 @@ class MigenPid(MigenModule):
             self.k_p_signal.eq(k_p),
             self.k_i_signal.eq(k_i),
         ]
+        # nur sum+int_sum need 30+16bit
+        
         self.sync += [
             self.error.eq(self.input_signal - self.setpoint_signal),
             self.prop.eq(self.error * self.k_p_signal),
             self.int.eq(self.error * self.k_i_signal),
             self.int_sum.eq(self.int_sum + self.int),
-            self.sum.eq(self.prop + self.int_sum),
-            self.out.eq(self.sum[0:width]),  # need to add saturation and bitshift to find a good dynamic range
+            #self.sum.eq( + self.int_sum),
+            #self.out.eq(self.sum[0:width]), #take the 16 # need to add saturation and bitshift to find a good dynamic range
+            #self.out.eq(self.sum),
+            self.out.eq(self.prop+self.int_sum[width:-1]),
+            #self.out.eq(self.sum>>16),
         ]
+
